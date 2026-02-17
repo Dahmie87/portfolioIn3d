c4e265e1f65039ef9747d61badb740f26ac8a6d9
dahmie <omotayodamilare07@gmail.com>
2026-02-17 17:07:30 +0100

    update abs

--- src/components/abstract.tsx
+++ src/components/abstract.tsx
@@ -14,15 +14,15 @@ type Position = { top: string; left: string };
 
 // ─── Constants ───────────────────────────────────────
 const STACKS: string[] = [
-  "React",
-  "Python",
-  "TypeScript",
-  "Docker",
-  "Next Js",
-  "Fastapi",
-  "AI & Automation",
+  "Kubernetes",
+  "Archictecture",
+  "MCP",
+  "n8n",
+  "CI/CD",
+  "FastApi",
+  "Hugging Face",
   "LLMs",
-  "API & Testing",
+  "LangChain",
   "GraphQL",
 ];
 
@@ -42,7 +42,7 @@ const COLORS: string[] = [
 const POSITIONS: Position[] = [
   { top: "4%", left: "52%" }, //top right?
   { top: "62%", left: "55%" }, //dowm rite!!
-  { top: "22%", left: "-14%" }, // top left
+  { top: "22%", left: "14%" }, // top left
   { top: "70%", left: "8%" }, // down lefty
 ];
 
