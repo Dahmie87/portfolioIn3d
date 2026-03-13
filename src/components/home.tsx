356b0a9c85175f7396d3e012c7b28d571cd1537a
dahmie <omotayodamilare07@gmail.com>
2026-03-13 12:17:17 +0100

    remove redundant home components

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,8 +1,6 @@
 import type React from "react";
 import AbstractShape from "./abstract";
 import heroProfile from "../assets/profile-1.jfif";
-import { FileText, Github, Linkedin, Mail } from "lucide-react";
-import BlurryText from "./projects";
 import NavBar from "./navbar";
 import About from "./about";
 import SocialIcons from "./social";
---
+++ src/components/projects.tsx
@@ -0,0 +1,25 @@
+export default function Project() {
+  return (
+    <section className="max-w-6xl mx-auto p-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+        {/* Left Column */}
+        <div className="font-normal text-4xl md:text-[42px]">
+          About Me
+        </div>
+
+        {/* Right Column */}
+        <div className="space-y-4 text-slate-500">
+          <p>
+            I’m a full-stack developer with a strong focus on backend engineering. I’m currently an undergraduate student at Lagos State University (LASU), majoring in Computer Science.
+          </p>
+          <p>
+            Over the past 3+ years, I’ve immersed myself in the world of software development, starting from basic programming concepts and gradually growing into complex system architecture and full-stack application design.
+          </p>
+          <p>
+            My primary backend tools are Python, Django, and FastAPI, while my frontend experience revolves around React, TypeScript, and Tailwind CSS.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
