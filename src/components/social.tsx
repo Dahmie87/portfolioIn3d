418ff08f8932c2a35137d03b34a6476701950379
dahmie <omotayodamilare07@gmail.com>
2026-03-18 16:57:55 +0100

    chore: add images

--- src/components/social.tsx
+++ src/components/social.tsx
@@ -31,6 +31,7 @@ const styles = `
   .social-icon-box:hover,
   .social-icon-box:focus {
     transform: translateY(-4px) scale(1.12);
+    margin:20ox;
     border-color: rgba(0,0,0,0.9);
     color: rgba(0,0,0,1);
     background: rgba(0,0,0,0.08);
@@ -90,7 +91,7 @@ function ResumeIcon() {
 
 function EmailIcon() {
   return (
-    <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
+    <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="0" strokeLinecap="round" strokeLinejoin="round">
       <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" />
       <polyline points="22,6 12,13 2,6" />
     </svg>
